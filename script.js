eba4e914624aed3f32b1b5ceea78efc4303bfd6b
anand-raj1 <pandeyanandraj12@gmail.com>
2023-12-10 23:15:50 +0530

    created a resume

---
+++ script.js
@@ -0,0 +1,21 @@
+const display = document.querySelector('.display');
+const input = document.querySelector('#upload');
+let img = document.querySelector("img");
+
+input.addEventListener('change',()=>{
+    let reader = new FileReader();
+    reader.readAsDataURL(input.files[0]);
+    reader.addEventListener('load',()=>{
+     display.innerHTML = `<img src= ${reader.result} alt=''/>`;   
+    })
+})
+
+
+
+
+
+
+var btn = document.querySelector("button");
+btn.onclick = () =>{
+    window.print();
+}
